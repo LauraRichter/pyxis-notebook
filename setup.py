8809a16932de86bf1d9271d10677ac2ca865158f
LauraRichter <laura@ska.ac.za>
2014-04-24 11:46:22 +0200

    Added setup.py
    Added setup.py for installing the package.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nbutils',
+      version='0.1',
+      description='Utilities for running Pyxis in the Ipython notebook',
+      author='Laura Richter',
+      author_email='laura@ska.ac.za',
+      url='https://github.com/LauraRichter/pyxis-pipeline.git',
+      packages=['nbutils'],
+      )
